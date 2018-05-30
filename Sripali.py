49d72b007db92f19ea7e32952bfe923d2314d326
aspriya <ashanpriyadarshana@gmail.com>
2018-05-30 19:25:09 +0530

    add phoneme_to_singlish method

--- Sripali.py
+++ Sripali.py
@@ -40,10 +40,10 @@ class SinhalaTransliterator(Frame):
 		self.singlish = Text(self, width = 50, height = 7, wrap=WORD)
 		self.singlish.grid(row=7, column=1, sticky=E)
 
-		
+
 	def transliterate(self):
 		sin_text = self.sinhala.get(0.0, END)
-		
+
 		#process the sinhala uncode string
 		print(type(sin_text))
 		sin_text_list = list(sin_text)
@@ -52,50 +52,26 @@ class SinhalaTransliterator(Frame):
 
 		# sin_text_len_modified = self.length_modification(sin_text)
 		# print("from length modification :", sin_text_len_modified)
-		
+
 		#call convert_to_phoneme function and get the halfly transliterated string
-		mapped_text_list = self.convert_to_phoneme(sin_text)	
+		mapped_text_list = self.convert_to_phoneme(sin_text)
 
 
 		print("mapped text list: ",mapped_text_list)
 		print("mapped text list len :", len(mapped_text_list))
 
-		a_insearted_list = self.insert_a(sin_text_list, mapped_text_list)
+		a_inserted_list = self.insert_a(sin_text_list, mapped_text_list)
+		print( "last array:", a_inserted_list);
+		print("\n");
 
-		
+		singlish_list = self.phoneme_to_english(a_inserted_list);
+		print("singlish list", singlish_list);
+
 		self.singlish.delete(0.0, END)
-		self.singlish.insert(0.0, "".join(a_insearted_list))
+		self.singlish.insert(0.0, "".join(singlish_list))
 
 
-	
-	def insert_a(self, sin_text_list, mapped_text_list):
-		a_inserted_list = []
-		
-
-		for i, letter in enumerate(mapped_text_list):
-			a_inserted_list.append(letter)
-
-			print(i, ": ", letter, " -> ", sin_text_list[i])
-			
-			# rule 1 : dont insert /a/ after a wovel phoneme representative
-			if sin_text_list[i] in big_list:	
-				continue
-			
-			# rule 2 : don't insert /a/ after phoneme representatives of special unicode symbols in sripali_list.
-			elif sin_text_list[i+1] in sripali_list:
-				continue
-			
-			# handdle spaces
-			elif letter == ' ':
-				continue
-
-			# rule 3 : after other not filtered phoneme symbols, append /a/
-			else:
-				a_inserted_list.append('a')
-
-		return a_inserted_list
-
-	def convert_to_phoneme(self, word):	
+	def convert_to_phoneme(self, word):
 		word_in_phoneme = []
 		for letter in word:
 			if letter == u"ං":
@@ -115,7 +91,7 @@ class SinhalaTransliterator(Frame):
 			if letter == u"ඊ":
 				word_in_phoneme.append(u'i:')
 			if letter == u"උ":
-				word_in_phoneme.append(u'u')
+				word_in_phoneme.append(u'ʊ')
 			if letter == u"ඌ":
 				word_in_phoneme.append(u'u:')
 			if letter == u"ඍ":
@@ -149,7 +125,7 @@ class SinhalaTransliterator(Frame):
 			if letter == u"ඞ":
 				word_in_phoneme.append(u'ŋ')
 			if letter == u"ඟ":
-				word_in_phoneme.append(u'r')
+				word_in_phoneme.append(u'n̆')
 			if letter == u"ච":
 				word_in_phoneme.append(u'c')
 			if letter == u"ඡ":
@@ -157,7 +133,7 @@ class SinhalaTransliterator(Frame):
 			if letter == u"ජ":
 				word_in_phoneme.append(u'ɟ')
 			if letter == u"ඣ":
-				word_in_phoneme.append(u'ɟ')
+				word_in_phoneme.append(u'ɟh')
 			if letter == u"ඤ":
 				word_in_phoneme.append(u'ɲ')
 			if letter == u"ඥ":
@@ -177,7 +153,7 @@ class SinhalaTransliterator(Frame):
 			if letter == u"ණ":
 				word_in_phoneme.append(u'n')
 			if letter == u"ඬ":
-				word_in_phoneme.append(u'ɖ')
+				word_in_phoneme.append(u'nɖ')
 			if letter == u"ත":
 				word_in_phoneme.append(u't')
 			if letter == u"ථ":
@@ -249,7 +225,7 @@ class SinhalaTransliterator(Frame):
 			if letter == u"ෝ":
 				word_in_phoneme.append(u'o:')
 			if letter == u"ෞ":
-				word_in_phoneme.append(u'ou')
+				word_in_phoneme.append(u'aou')
 			if letter == u"ෟ":
 				word_in_phoneme.append(u'ou')
 			if letter == u"ෲ":
@@ -258,12 +234,220 @@ class SinhalaTransliterator(Frame):
 				word_in_phoneme.append(u'ou')
 			if letter == u" ":
 				word_in_phoneme.append(u' ')
-		
+
 		# word_in_phoneme = "".join(word_in_phoneme) #converting to string
 		return word_in_phoneme
 
+		def insert_a(self, sin_text_list, mapped_text_list):
+			a_inserted_list = []
 
-root = Tk()                      
+			for i, letter in enumerate(mapped_text_list):
+				a_inserted_list.append(letter)
+
+				print(i, ": ", letter, " -> ", sin_text_list[i])
+
+				# rule 1 : dont insert /a/ after a wovel phoneme representative
+				if sin_text_list[i] in big_list:
+					continue
+
+				# rule 2 : don't insert /a/ after phoneme representatives of special unicode symbols in sripali_list.
+				elif sin_text_list[i+1] in sripali_list:
+					continue
+
+				# handdle spaces
+				elif letter == ' ':
+					continue
+
+				# rule 3 : if the letter is not filtered by above rules, then append /a/
+				else:
+					a_inserted_list.append('a')
+
+			return a_inserted_list
+
+
+	def insert_a(self, sin_text_list, mapped_text_list):
+		a_inserted_list = []
+
+		for i, letter in enumerate(mapped_text_list):
+			a_inserted_list.append(letter)
+
+			print(i, ": ", letter, " -> ", sin_text_list[i])
+
+			# rule 1 : dont insert /a/ after a wovel phoneme representative
+			if sin_text_list[i] in big_list:
+				continue
+
+			# rule 2 : don't insert /a/ after phoneme representatives of special unicode symbols in sripali_list.
+			elif sin_text_list[i+1] in sripali_list:
+				continue
+
+			# handdle spaces
+			elif letter == ' ':
+				continue
+
+			# rule 3 : if the letter is not filtered by above rules, then append /a/
+			else:
+				a_inserted_list.append('a')
+
+		return a_inserted_list
+
+
+	def phoneme_to_english(self, word):
+		word_in_singlish = []
+		for letter in word:
+			if letter == u"ŋ":
+				word_in_singlish.append(u'n') #some times to "ng"
+			if letter == u"h":
+				word_in_singlish.append(u'h')
+			if letter == u"a":
+				word_in_singlish.append(u'a')  #vowel
+			if letter == u"a:":
+				word_in_singlish.append(u'a') #vowel
+			if letter == u"æ":
+				word_in_singlish.append(u'e')  #This is special case. although it should be "a", in singlish we use "e"
+				                              # For example ඇල්ල -> Ella
+			if letter == u"æ:":
+				word_in_singlish.append(u'e') # කුරුණෑගල -> kurunEgala
+			if letter == u"i":
+				word_in_singlish.append(u'i')
+			if letter == u"i:":
+				word_in_singlish.append(u'ee') #some times "ea"
+			if letter == u"u":
+				word_in_singlish.append(u'u')
+			if letter == u"u:":
+				word_in_singlish.append(u'oo') # some times "ue"
+			if letter == u"ri":
+				word_in_singlish.append(u'ri')
+			if letter == u"ru:":
+				word_in_singlish.append(u'ruu')
+			if letter == u"ilu":
+				word_in_singlish.append(u'ilu')
+			if letter == u"ilu:":
+				word_in_singlish.append(u'ilue')
+			if letter == u"e":
+				word_in_singlish.append(u'e')
+			if letter == u"e:":
+				word_in_singlish.append(u'a') #ate, a-level, rate, late  : so all use "a" for ඒ
+			if letter == u"ai":
+				word_in_singlish.append(u'ai')
+			if letter == u"o":
+				word_in_singlish.append(u'o')
+			if letter == u"o:":
+				word_in_singlish.append(u'o')  #ex: හෝමාගම  -> homagama
+			if letter == u"au":
+				word_in_singlish.append(u'au')
+			if letter == u"k":
+				word_in_singlish.append(u'k')
+				continue
+			if letter == u"k":
+				word_in_singlish.append(u'k')
+			if letter == u"ɡ":
+				word_in_singlish.append(u'g')
+				continue
+			if letter == u"ɡ":
+				word_in_singlish.append(u'g')
+			if letter == u"ŋ":
+				word_in_singlish.append(u'n')
+			if letter == u"n̆":
+				word_in_singlish.append(u'n')
+			if letter == u"c":
+				word_in_singlish.append(u'c')
+				continue
+			if letter == u"c":
+				word_in_singlish.append(u'c')
+			if letter == u"ɟ":
+				word_in_singlish.append(u'j')
+			if letter == u"ɟh":
+				word_in_singlish.append(u'jh')
+			if letter == u"ɲ":
+				word_in_singlish.append(u'kn')
+			if letter == u"jɲ":
+				word_in_singlish.append(u'gn')
+			if letter == u"ʄ":
+				word_in_singlish.append(u'nj')
+			if letter == u"ʈ":
+				word_in_singlish.append(u't')
+				continue
+			if letter == u"ʈ":
+				word_in_singlish.append(u't')
+			if letter == u"ɖ":
+				word_in_singlish.append(u'd')
+				continue
+			if letter == u"ɖ":
+				word_in_singlish.append(u'd')
+			if letter == u"n":
+				word_in_singlish.append(u'n')
+				continue
+			if letter == u"n":
+				word_in_singlish.append(u'n')
+			if letter == u"nɖ":
+				word_in_singlish.append(u'nd')
+			if letter == u"t":
+				word_in_singlish.append(u'th')
+				continue
+			if letter == u"t":
+				word_in_singlish.append(u'th')
+			if letter == u"d":
+				word_in_singlish.append(u'd')
+				continue
+			if letter == u"d":
+				word_in_singlish.append(u'd')
+			if letter == u"ɗ":
+				word_in_singlish.append(u'nd')
+			if letter == u"p":
+				word_in_singlish.append(u'p')
+				continue
+			if letter == u"p":
+				word_in_singlish.append(u'p')
+			if letter == u"b":
+				word_in_singlish.append(u'b')
+				continue
+			if letter == u"b":
+				word_in_singlish.append(u'b')
+			if letter == u"m":
+				word_in_singlish.append(u'm')
+			if letter == u"ɓ":
+				word_in_singlish.append(u'b')
+			if letter == u"j":
+				word_in_singlish.append(u'y')
+			if letter == u"r":
+				word_in_singlish.append(u'r')
+			if letter == u"l":
+				word_in_singlish.append(u'l')
+				continue
+			if letter == u"w":
+				word_in_singlish.append(u'w')
+			if letter == u"ʃ":
+				word_in_singlish.append(u'sh')
+				continue
+			if letter == u"ʃ":
+				word_in_singlish.append(u'sh')
+			if letter == u"s":
+				word_in_singlish.append(u's')
+			if letter == u"h":
+				word_in_singlish.append(u'h')
+			if letter == u"l":
+				word_in_singlish.append(u'l')
+			if letter == u"f":
+				word_in_singlish.append(u'f')
+			if letter == u"_ ":
+				word_in_singlish.append(u'_')
+
+			if letter == u"aou":
+				word_in_singlish.append(u'au')
+			if letter == u"ou":
+				word_in_singlish.append(u'ou')
+				continue
+			if letter == u"ru:":
+				word_in_singlish.append(u'ru')
+			if letter == u" ":
+				word_in_singlish.append(u' ')
+
+		# word_in_phoneme = "".join(word_in_phoneme) #converting to string
+		return word_in_singlish
+
+
+root = Tk()
 root.title("Welcome to the System")
 root.geometry("600x400")
 app = SinhalaTransliterator(root)
